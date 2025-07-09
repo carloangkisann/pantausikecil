244bb3afe31e1acea04f398cc08920d9eb67ef74
carllix <carloangkisan21@gmail.com>
2025-07-09 12:47:00 +0700

    chore: change non to supabase and install some dependencies

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,7 +1,14 @@
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from 'pg';
 import { ENV } from "./env";
 import * as schema from "../db/schema";
 
-const sql = neon(ENV.DATABASE_URL || "");
-export const db = drizzle(sql, { schema });
+if (!ENV.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined in environment variables.");
+}
+
+const pool = new Pool({
+  connectionString: ENV.DATABASE_URL,
+});
+
+export const db = drizzle(pool, { schema });
